fd377ae44aa3a9826d19eefdc1235a3c1ea0f292
Gustavo Silvera <grs5382@gmail.com>
2022-04-21 09:43:57 -0400

    adding somewhat decent throttle model

--- models.py
+++ models.py
@@ -18,3 +18,22 @@ class SteeringModel(torch.nn.Module):
 
     def forward(self, x):
         return self.network(x)
+
+
+class ThrottleModel(torch.nn.Module):
+    def __init__(self, in_dim: int):
+        super().__init__()
+        self.in_dim = in_dim
+        self.out_dim = 1  # outputting only a single scalar
+        layers = [
+            torch.nn.Linear(self.in_dim, 128),
+            torch.nn.Linear(128, 256),
+            torch.nn.Linear(256, 256),
+            torch.nn.ReLU(),  # only positive
+            torch.nn.Linear(256, self.out_dim),
+        ]
+        self.network = torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        # throttle should be always positive
+        return self.network(x)
